7e4ebc774fa922715a35459115e3e58a14dae151
David Baughman <dbaughman@us.imshealth.com>
2016-01-07 16:10:38 -0800

    More work on docs, adding gulp task for Theo

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require( 'gulp' );
 var sass = require( 'gulp-sass' );
 var autoprefixer = require( 'gulp-autoprefixer' );
 var browserSync = require( 'browser-sync' );
+var theo = require( 'theo' );
 
 
 gulp.task( 'serve', [ 'default' ], function() {
@@ -9,15 +10,28 @@ gulp.task( 'serve', [ 'default' ], function() {
     server: 'dist'
   });
 
-  gulp.watch( 'scss/**/*.scss', [ 'styles' ] );
-  gulp.watch( 'docs/**/*', [ 'docs' ] );
+  gulp.watch( 'scss/**/*.scss', [ 'apollo-styles' ] );
+  gulp.watch( 'docs/**/*.html', [ 'docs' ] );
+  gulp.watch( 'docs/_scss/**/*.scss', [ 'docs-styles' ] );
 });
 
 
-gulp.task( 'styles', function () {
+gulp.task( 'apollo-styles', function () {
   gulp.src( 'scss/apollo.scss' )
     .pipe( sass({ 
         includePaths: [ 'node_modules' ],
+        outputStyle: 'expanded'  // expanded for development
+      })
+      .on( 'error', sass.logError ))
+    .pipe( gulp.dest( 'dist/css/' ) )
+    .pipe( browserSync.stream() );
+});
+
+
+gulp.task( 'docs-styles', function () {
+  gulp.src( 'docs/_scss/docs.scss' )
+    .pipe( sass({ 
+        includePaths: [ 'node_modules', 'scss' ],
         outputStyle: 'expanded'  // expanded for development
       })
       .on( 'error', sass.logError ))
@@ -43,4 +57,12 @@ gulp.task( 'jekyll', function ( gulpCallBack ) {
 });
 
 
-gulp.task( 'default', [ 'styles', 'docs' ] );
+gulp.task( 'theo', function() {
+  gulp.src( '_colors.json')
+    .pipe( theo.plugins.transform( 'web' ))
+    .pipe( theo.plugins.format( 'scss' ))
+    .pipe( gulp.dest( 'scss' ));
+});
+
+
+gulp.task( 'default', [ 'apollo-styles', 'docs-styles', 'docs' ] );
