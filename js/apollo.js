b2d651f7ee8053de4c18c53e8c6dfa360ddb2a76
David Baughman <dbaughman@us.imshealth.com>
2016-09-22 14:14:03 -0700

    Reverted to including all bootstrap plugins and adding tether.js

--- js/apollo.js
+++ js/apollo.js
@@ -2,18 +2,11 @@
  * Apollo JS v1.0.0-beta.4
  */
 
-
 ///
 /// Bootstrap jQuery Plugins
 ///
 
-require( '../node_modules/bootstrap/js/dist/alert' );
-require( '../node_modules/bootstrap/js/dist/carousel' );
-require( '../node_modules/bootstrap/js/dist/collapse' );
-require( '../node_modules/bootstrap/js/dist/dropdown' );
-require( '../node_modules/bootstrap/js/dist/modal' );
-require( '../node_modules/bootstrap/js/dist/tab' );
-require( '../node_modules/bootstrap/js/dist/util' );
+require( '../node_modules/bootstrap/dist/js/bootstrap' );
 
 
 ///
